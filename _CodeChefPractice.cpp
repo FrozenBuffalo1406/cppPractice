f20861def25ac9e87655d3c613fe02b07f0b4e26
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-09-04 19:57:06 +0700

    editing 'WA test Case'

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -1,19 +1,41 @@
 #include <iostream>
 using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    int t;
+    int t, ans;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int x;
-        cin >> x;
-        int arr[x];
-        for (int j = 0; j < x; j++)
+        int tcCount;
+        cin >> tcCount;
+        int *arr = new int[tcCount];
+        int *sum = new int[tcCount];
+        for (int i = 0; i < tcCount; i++)
         {
             cin >> arr[i];
         }
-    }
+        string tcStat;
+        cin >> tcStat;
+        for (int i = 0; i < tcStat.length(); ++i)
+        {
+            if (tcStat.at(i) == '0')
+            {
+                sum[i] = arr[i];
+            }
+        }
 
+        int min = sum[0];
+        int size = sizeof(sum) / sizeof(sum[0]);
+        for (int i = 0; i < size; i++)
+        {
+            if (sum[i] < min)
+            {
+                min = sum[i];
+            }
+            ans = min;
+        }
+        cout << ans;
+    }
     return 0;
 }
