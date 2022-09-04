9a326e4ac9c1d0c8b8f6acca8b13749f228beddd
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-09-05 01:50:53 +0700

    solved _CodeChefPractice.cpp 'WA Test Cases'

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <string.h>
 using namespace std;
 
 int main()
@@ -8,34 +8,28 @@ int main()
     cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int t;
-        cin >> t;
-        for (int i = 0; i < t; i++)
+        int n;
+        int min;
+        string b;
+        cin >> n;
+        int *a = new int[n];
+        for (int i = 0; i < n; i++)
         {
-
-            int n;
-            cin >> n;
-            int *a = new int[n];
-            for (int i = 0; i < n; i++)
+            cin >> a[i];
+        }
+        min = a[0];
+        cin >> b;
+        for (int i = 0; i < b.size(); i++)
+        {
+            if (b[i] == '0')
             {
-                cin >> a[i];
-            }
-            string b;
-            cin >> b;
-            int min = a[0];
-            for (int i = 0; i < b.size(); i++)
-            {
-                if (b[i] == '0')
+                if (min > a[i])
                 {
-                    if (min > a[i])
-                    {
-                        min = a[i];
-                    }
+                    min = a[i];
                 }
             }
-            cout << min << endl;
         }
-
-        return 0;
+        cout << min << endl;
     }
+    return 0;
 }
