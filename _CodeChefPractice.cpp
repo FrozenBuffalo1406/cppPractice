5e2ab4bbb947f2c269a13f32a36d0f5be02e7b06
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-09-08 09:01:15 +0700

    editing _codeChefPractice.cpp [error at true false filter]

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
-
 int main()
 {
-    int t, min;
+    int min;
+    int t;
+    int n = INT_MAX;
+    string v;
+    // 5
+    // 3
+    // 5 10 3
+    // 000
+    // 3
+    // 5 10 3
+    // 001
+    // 3
+    // 5 5 3
+    // 001
+    // 3
+    // 5 5 3
+    // 101
+    // 5
+    // 10 100 100 10 10
+    // 00001
+    cin >> t;
     for (int i = 0; i < t; i++)
     {
-        int n;
+        min = INT_MAX;
         cin >> n;
+        int a[n];
         for (int i = 0; i < n; i++)
         {
-            auto a = new int[n];
             cin >> a[i];
-            min = a[0];
         }
-        string v;
         cin >> v;
-
-        for (int i = 0; i < n; i++)
+        for (int i = 0; i < v.length(); i++)
         {
             if (v[i] == '0')
             {
-                if (min > v[i])
+                if (a[i] < min)
                 {
-                    min = v[i];
+                    min = a[i];
                 }
             }
         }
     }
+
     cout << min;
     return 0;
 }
