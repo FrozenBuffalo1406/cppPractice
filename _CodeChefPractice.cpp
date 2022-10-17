10b022fbd211136e4523560ef146766a38983c02
Bakedrice14 <ahmadfauzirizky003@gmail.com>
2022-10-17 21:53:27 +0700

    adding git ignore

--- _CodeChefPractice.cpp
+++ _CodeChefPractice.cpp
@@ -1,35 +1,7 @@
-#include <iostream>
 #include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-    int min;
-    int t;
-    int n = INT_MAX;
-    string v;
-    cin >> t;
-    for (int i = 0; i < t; i++)
-    {
-        min = INT_MAX;
-        cin >> n;
-        int a[n];
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-        cin >> v;
-        for (int i = 0; i < v.length(); i++)
-        {
-            if (v[i] == '0')
-            {
-                if (a[i] < min)
-                {
-                    min = a[i];
-                }
-            }
-        }
-        cout << min << endl;
-    }
 
+#include <iostream>
+using namespace std;
+int main() {
     return 0;
 }
--- _TOKI_GO_GO_GO.cpp
+++ _TOKI_GO_GO_GO.cpp
@@ -1,39 +1,28 @@
+#include <stdlib.h>
+
 #include <iostream>
+
 using namespace std;
-void isTruePrint(bool isTrue)
-{
-  if (isTrue == false)
-  {
-    cout << "bukan" << endl;
-  }
-  else
-  {
-    cout << "ya" << endl;
-  }
-}
-main()
-{
-  int t;
-  cin >> t;
-  for (int i = 0; i < t; i++)
-  {
-    int a;
-    bool isPrime = true;
-    cin >> a;
-    for (int i = 0; i < a; i++)
-    {
-      bool isPrime = true;
-      if (a % i == 0)
-      {
-        isPrime = true;
-        break;
-      }
-      else
-      {
-        isPrime = false;
-        break;
-      }
+
+int main() {
+    double a, b, c, d;
+    double tot1, tot2;
+    int jlh;
+
+    cin >> jlh;
+    for (int i = 0; i < jlh; i++) {
+        cin >> a >> b >> c >> d;
+        tot1 = 1 / (c / a);
+        tot2 = 1 / (d / b);
+        if (tot1 > tot2) {
+            cout << "Chefina" << endl;
+        } else if (tot1 < tot2) {
+            cout << "Chef" << endl;
+        } else if (tot1 == tot2) {
+            cout << "Both" << endl;
+        }
     }
-    isTruePrint(isPrime);
-  }
+
+    cin.get();
+    return 0;
 }
